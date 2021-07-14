82733fc5e29e95611ee697abf901c8e96e42bc3e
mikhaylov-yv <dezmond22@yandex.ru>
2021-07-14 21:11:18 +0300

    Переделал под ООП

--- bot_app/bar_serch.py
+++ bot_app/bar_serch.py
@@ -6,6 +6,7 @@ class Bar_serch:
     def __init__(self, location_lat = 60, location_lng = 30):
         self.location = (location_lat, location_lng)
         self.gmaps = googlemaps.Client(key = bot_data.google_token)
+        self.i = 0
 
     def serch(self):
         data = self.gmaps.places(type = "bar",
@@ -15,14 +16,16 @@ class Bar_serch:
                                                  'rating', 'types', 'user_ratings_total',
                                                  'opening_hours.open_now',
                                                  'geometry.location.lat', 'geometry.location.lng', 'place_id']]
-        self.get_reviews()
+        self.i_max = self.df.index[-1]
+        self.get_bar()
 
-        return self.df
+        return self
 
     # Добавляем последний худший коментарий
-    def get_reviews(self):
-        for place_id in self.df.place_id:
-            bar_info_df = self.gmaps.place(place_id = place_id, language = 'ru')
-            reviews_df = pd.json_normalize(bar_info_df['result']['reviews'])
-            review = reviews_df[reviews_df.rating == reviews_df.rating.min()].text.values[0]
-            self.df.loc[self.df.place_id == place_id, 'review'] = review
+    def get_bar(self):
+        place_id = self.df.place_id[self.i]
+        bar_info_df = self.gmaps.place(place_id = place_id, language = 'ru')
+        reviews_df = pd.json_normalize(bar_info_df['result']['reviews'])
+        review = reviews_df[reviews_df.rating == reviews_df.rating.min()].text.values[0]
+        self.df.loc[self.df.place_id == place_id, 'review'] = review
+        return self.df.loc[self.df.place_id == place_id].squeeze()
--- bot_app/bot.py
+++ bot_app/bot.py
@@ -1,4 +1,5 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 import bot_data
 import bar_serch
@@ -11,47 +12,51 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text= 'Сейчас мы пройдемся по барам пришли свою геолокацию\nя пришлю тебе 5 мест поблизости')
+class Bot:
+    def start(self, update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text= 'Сейчас мы пройдемся по барам пришли свою геолокацию\nя пришлю тебе 5 мест поблизости')
 
 
-def mesag(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Ты пидор')
+    def mesag(self, update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Ты пидор')
 
-def location(update, context):
-    loc_data = update.message.effective_attachment
+    def location(self, update, context):
+        loc_data = update.message.effective_attachment
 
-    serch = bar_serch.Bar_serch(loc_data.latitude, loc_data.longitude)
+        serch = bar_serch.Bar_serch(loc_data.latitude, loc_data.longitude)
 
-    df = serch.serch()
-    # context.bot.send_message(chat_id=update.effective_chat.id,
-    #                         text=f"Вот {i} бара рядом", parse_mode='Markdown')
-    for i, ind in enumerate(df.index):
-        name = df.loc[ind, 'name']
-        latitude = df.loc[ind, 'geometry.location.lat']
-        longitude = df.loc[ind, 'geometry.location.lng']
+        self.bars = serch.serch()
+        self.get_bar(update, context)
+
+
+
+    def get_bar(self, update, context):
+        bar = self.bars.get_bar()
+        name = bar['name']
+        review = bar['review']
+        latitude = bar['geometry.location.lat']
+        longitude = bar['geometry.location.lng']
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text=name, parse_mode='Markdown', disable_notification = False)
         context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=df.loc[ind, 'review'], parse_mode='Markdown', disable_notification=False)
+                                 text=review, parse_mode='Markdown', disable_notification=False)
         context.bot.sendLocation(chat_id=update.effective_chat.id,
                                  latitude = latitude, longitude = longitude, disable_notification = False)
 
-    #     Тут будет условие выхода из цикла
-        break
+        keyboard = [[
+                InlineKeyboardButton("Еще!!!", callback_data='1'),
+                InlineKeyboardButton("Идем сюда", callback_data='2'),
+                ]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text('И так!', reply_markup=reply_markup, disable_notification = False)
 
 
 
-
-
-    print('геолокация')
-
-
-
-start_handler = CommandHandler('start', start)
-mesag_handler = MessageHandler(Filters.text & (~Filters.command) & (~Filters.location), mesag)
-location_handler = MessageHandler(Filters.location, location)
+bot = Bot()
+start_handler = CommandHandler('start', bot.start)
+mesag_handler = MessageHandler(Filters.text & (~Filters.command) & (~Filters.location), bot.mesag)
+location_handler = MessageHandler(Filters.location, bot.location)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(mesag_handler)
