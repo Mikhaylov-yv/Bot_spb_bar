4cffbd6b9527b3486deef505018e9891e7c42e75
mikhaylov-yv <dezmond22@yandex.ru>
2022-05-15 16:22:36 +0300

    Добавил постоянный перезапуск

--- setap.py
+++ setap.py
@@ -4,8 +4,6 @@ sh.git.pull('origin')
 
 import sys
 sys.path.append('bot_app')
-while True:
-    try:
-        import bot_app.bot
-    except:
-        print('Какая-то ошибка')
+
+
+import bot_app.bot
